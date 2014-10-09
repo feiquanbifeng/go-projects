2c0d31d14cba1ca92b76c40d463e03d0d622a9c2
shoyou <460429192@qq.com>
2014-10-09 14:15:14 +0800

    Update commander.go

--- 2014/commander/commander.go
+++ 2014/commander/commander.go
@@ -3,6 +3,7 @@ package main
 import (
     "flag"
     "fmt"
+    "os"
     "strings"
 )
 
@@ -24,6 +25,90 @@ func (o *Option) Name() string {
 // Check if `arg` matches the short or long flag
 func (o *Option) Is(arg string) bool {
     return arg == o.short || arg == o.long
+}
+
+type Command struct {
+    commands []string
+    options  []Option
+    execs    []string
+    args     []string
+    name     string
+    version  string
+}
+
+func (c *Command) command(name, desc string) {
+    args := strings.Fields(name)
+    // cmd := &
+}
+
+func (c *Command) addImplicitHelpCommand() {
+    c.command("help [cmd]", "display help for [cmd]")
+}
+
+func (c *Command) parseExpectedArgs(args string) {
+}
+
+// Return an option matching `arg` if any
+func (c *Command) OptionFor(arg) Option {
+    for _, v := range c.options {
+        if v.Is(arg) {
+            return v
+        }
+    }
+    return nil
+}
+
+// Return an object containing options as key-vale pairs
+func (c *Command) Opts() map[string]string {
+    result := make(map[string]string, len(c.options))
+    for _, v := range c.options {
+        key := v.Name()
+        if key == "version" {
+            result[key] = c.version
+        } else {
+            // result[key] =
+        }
+    }
+    return result
+}
+
+// Unknown option `flag`
+func (c *Command) unknownOption(flag string) {
+    fmt.Fprintf(os.Stderr, " error: unknown option %s", flag)
+    os.Exit(1)
+}
+
+// Set the program version to `str`.
+func (c *Command) Version(args ...string) string {
+    if len(args) == 0 {
+        return c.version
+    }
+    c.version = args[0]
+    flags := "-V, --version"
+    if len(args) == 2 {
+        flags = args[1]
+    }
+}
+
+// Set the description to `str`.
+func (c *Command) Description(args ...string) string {
+    if len(args) == 0 {
+        return ""
+    }
+    return ""
+}
+
+// Set an alias for the command
+func (c *Command) Alias(args ...string) string {
+    if len(args) == 0 {
+        return ""
+    }
+    return ""
+}
+
+// Set / Get the command usage `str`
+func (c *Command) Usage(str string) {
+
 }
 
 func main() {
