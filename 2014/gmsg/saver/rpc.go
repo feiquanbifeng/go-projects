8d2476d5f1caab3c9b56442594565d7efa1b5743
shoyou <460429192@qq.com>
2014-10-13 15:31:31 +0800

    Create rpc.go

---
+++ 2014/gmsg/saver/rpc.go
@@ -0,0 +1,38 @@
+// RPC operator
+// listen port 1234
+
+package saver
+
+import (
+    "log"
+    "net"
+    "net/http"
+    "net/rpc"
+)
+
+var port = ":1234"
+
+func init() {
+    server := new(Server)
+    rpc.Register(server)
+    rpc.HandleHTTP()
+    l, e := net.Listen("tcp", port)
+    if e != nil {
+        log.Fatal("listen error:", e)
+    }
+    go http.Serve(l, nil)
+}
+
+type Message struct {
+    Id  int
+    Msg string
+}
+
+type Server struct{}
+
+func (s *Server) AddMessage(args *Message, result *string) error {
+    c := NewCommand()
+    c.zadd(args.Id, args.Msg)
+    c.close()
+    return nil
+}
