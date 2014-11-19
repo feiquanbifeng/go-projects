3fc2ecac0ed92d6eb19c38bfa81c3f543c3068ec
shoyou <460429192@qq.com>
2014-11-19 11:39:59 +0800

    Update parents.go

--- 2014/parents/parents.go
+++ 2014/parents/parents.go
@@ -1,102 +1,105 @@
-// refer to
-// 
+// refer to nodejs path-parents
+// just simple implements
 
-package main
+package parents
 
 import (
-	"os"
-	"runtime"
-	"fmt"
-	"path"
-	"strings"
-	"regexp"
-	"array"
+    "os"
+    "runtime"
+    "fmt"
+    "path"
+    "strings"
+    "regexp"
+    "array"
 )
 
-func Parents(cwd string, opts ...string) []string {
-	if cwd == "" {
-		cwd, _ = os.Getwd()
-	}
-	var (
-		isWindows bool
-		sep string
-		reg *regexp.Regexp
-		c string
-		init array.Array
-	)
-	if len(opts) == 0 {
-		isWindows = runtime.GOOS == "windows"
-	} else {
-		isWindows = strings.HasPrefix(opts[0], "win")
-	}
-	if isWindows {
-		c = `[\\\/]`
-		init = array.Array{""}
-	} else {
-		c = `/`
-		init = array.Array{"/"}
-	}
+// Public method to provider API
+// Actually you could not pass method's parameters
+func Parents(args ...string) []string {
+    var (
+        isWindows = runtime.GOOS == "windows"
+        reg *regexp.Regexp
+        init array.Array
+        cwd string
+        sep string
+        c string
+    )
+    if n := len(args); n == 0 {
+        cwd, _ = os.Getwd()
+    } else if n == 1 {
+        cwd = args[0]
+    } else {
+        isWindows = strings.HasPrefix(args[1], "win")
+    }
 
-	reg, _ = regexp.Compile(c)
+    if isWindows {
+        c = `[\\\/]`
+        init = array.Array{""}
+    } else {
+        c = `/`
+        init = array.Array{"/"}
+    }
 
-	var join = func(x, y interface{}) array.Array {
-		tmpArray := array.Array{x, y}
-		var ps = tmpArray.Filter(func(p interface{}, args ...interface{}) bool {
-			switch p.(type) {
-			case string:
-				if p.(string) != "" {
-					return true
-				}
-				return false
-			default:
-				return false
-			}
-			return false
-		})
-		if isWindows {
-			sep = "\\"
-		} else {
-			sep = "/"
-		}
-		return array.Array{path.Clean(ps.Join(sep))}
-	}
+    reg = regexp.MustCompile(c)
 
-	var res = path.Clean(cwd)
-	rr := array.Array{"\\F:", "Go", "1110"}
-	reg.Split(res, 5)
-	arrReduce := rr.Reduce(func(acc, dir interface{}, ix ...interface{}) interface{} {
-		tmpAcc := acc.(array.Array)
-		index := ix[0].(int)
-		tmpAcc = tmpAcc.Concat(join(tmpAcc[index], dir))
-		return tmpAcc
-	}, init)
+    var join = func(x, y interface{}) array.Array {
+        tmpArray := array.Array{x, y}
+        var ps = tmpArray.Filter(func(p interface{}, args ...interface{}) bool {
+            switch p.(type) {
+            case string:
+                if p.(string) != "" {
+                    return true
+                }
+                return false
+            default:
+                return false
+            }
+            return false
+        })
+        if isWindows {
+            sep = "\\"
+        } else {
+            sep = "/"
+        }
+        return array.Array{path.Clean(ps.Join(sep))}
+    }
 
-	arrSlice := arrReduce.(array.Array)
-	arrReverse := arrSlice.Slice(1, 0)
-	arrReverse.Reverse()
-	fmt.Println(arrReverse)
+    var res = path.Clean(cwd)
+    arr := array.Array{}
+    for _, v := range reg.Split(res, -1) {
+        arr.Push(v)
+    }
 
-	if arrReverse[0] == arrReverse[1] {
-		return []string{arrReverse[0].(string)}
-	}
+    arrReduce := arr.Reduce(func(acc, dir interface{}, ix ...interface{}) interface{} {
+        tmpAcc := acc.(array.Array)
+        index := ix[0].(int)
+        tmpAcc = tmpAcc.Concat(join(tmpAcc[index], dir))
+        return tmpAcc
+    }, init)
 
-	if isWindows && strings.HasPrefix(cwd, "\\") {
-		cut := arrReverse.Slice(0, -1)
-		cut.Map(func(d interface{}, args ...interface{}) interface{} {
-			var ch = d.(string)[0]
-			if ch == '\\' {
-				return d
-			} else if ch == '.' {
-				return "\\" + d.(string)[1:]
-			} else {
-				return "\\" + d.(string)
-			}
-		})
-		fmt.Println(([]string)(cut))
-	}
-	return nil
-}
+    // Not like javascript support method links
+    // everytime you should assign
+    arrSlice := arrReduce.(array.Array)
+    arrReverse := arrSlice.Slice(1, 0)
+    arrReverse.Reverse()
 
-func main() {
-	fmt.Println(Parents("\\F:\\Go\\1110"))
+    if arrReverse[0] == arrReverse[1] {
+        return []string{arrReverse[0].(string)}
+    }
+
+    if isWindows && strings.HasPrefix(cwd, "\\") {
+        cut := arrReverse.Slice(0, -1)
+        cut.Map(func(d interface{}, args ...interface{}) interface{} {
+            var ch = d.(string)[0]
+            if ch == '\\' {
+                return d
+            } else if ch == '.' {
+                return "\\" + d.(string)[1:]
+            } else {
+                return "\\" + d.(string)
+            }
+        })
+        return cut.ToString()
+    }
+    return arrReverse.ToString()
 }
